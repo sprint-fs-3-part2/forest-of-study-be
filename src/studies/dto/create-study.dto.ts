fa832bca5a2dc03f3a5c72aa88257dbce8798eeb
ChoboDev <gealot@gmail.com>
2024-11-19 15:45:05 +0900

    Chore(package): resolve confilct

--- src/studies/dto/create-study.dto.ts
+++ src/studies/dto/create-study.dto.ts
@@ -41,7 +41,7 @@ export class CreateStudyDto {
 }
 
 // CreateStudyResponseDto는 한 개의 스터디 생성 시 클라이언트에 전달할(응답) 데이터 형식을 정의한 객체
-// 스터디 생성이 완료되면 클라이언트에게 해당 스터디의 ID를 전달함
+// 스터디 생성이 완료되면 클라이언트에게 해당 스터디의 ID를 전달
 // 이후 클라이언트는 해당 스터디 ID를 이용하여 스터디 상세 페이지에 필요한 정보를 요청함
 // 유저 플로우 : 스터디 만들기 Page -> 스터디 상세 Page
 export class CreateStudyResponseDto {
