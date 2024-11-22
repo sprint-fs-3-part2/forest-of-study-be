89f500a4ebeb77c1a3fbf3aff2161d142ed935cc
taehoone <ghost111789@gmail.comm>
2024-11-22 11:39:14 +0900

    오늘의 습관 진행중 커밋

--- src/habits/dto/create-habit.dto.ts
+++ src/habits/dto/create-habit.dto.ts
@@ -1,1 +1,20 @@
-export class CreateHabitDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Exclude } from 'class-transformer';
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class CreateHabitDto {
+  @ApiProperty({
+    nullable: false,
+    description: '',
+    example: '',
+    type: String,
+  })
+  
+}
+@ApiProperty({
+  nullable: false,
+  description: '',
+  example: '',
+  type: String,
+})
+
--- src/habits/habits.controller.ts
+++ src/habits/habits.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { HabitsService } from './habits.service';
 import { CreateHabitDto } from './dto/create-habit.dto';
 import { UpdateHabitDto } from './dto/update-habit.dto';
@@ -7,6 +15,17 @@ import { UpdateHabitDto } from './dto/update-habit.dto';
 export class HabitsController {
   constructor(private readonly habitsService: HabitsService) {}
 
+  @ApiCustomDocs({
+    summary: '습관 생성',
+    description: {
+      title: '습관를 생성합니다.',
+      contents: [
+        // '스터디 이름, 스터디 개설자 닉네임, 스터디 소개, 배경화면, 비밀번호를 입력받아 스터디를 생성합니다.',
+      ],
+    },
+    // requestType: CreateStudyDto,
+    // responseType: CreateStudyResponseDto,
+  })
   @Post()
   create(@Body() createHabitDto: CreateHabitDto) {
     return this.habitsService.create(createHabitDto);
