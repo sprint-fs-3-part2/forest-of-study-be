e3f91c8b308e160055428eac7fdab071327c4e47
heonq <gkagjsrb@naver.com>
2024-11-24 16:08:06 +0900

    🌱 feat: 습관 수정 서비스 구현

--- src/habits/habits.service.ts
+++ src/habits/habits.service.ts
@@ -8,6 +8,10 @@ import {
   CreateHabitsResponseDto,
 } from './dto/create-habit.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import {
+  UpdateHabitsDto,
+  UpdateHabitsResponseDto,
+} from './dto/update-habit.dto';
 
 @Injectable()
 export class HabitsService {
@@ -50,5 +54,47 @@ export class HabitsService {
     );
 
     return CreateHabitsResponseDto.of(createdHabits);
+  }
+  async updateHabits(
+    studyId: string,
+    updateHabitsDto: UpdateHabitsDto,
+  ): Promise<UpdateHabitsResponseDto> {
+    const existingHabits = await this.prisma.habit.findMany({
+      where: {
+        id: { in: updateHabitsDto.habits.map((h) => h.id) },
+        studyId,
+      },
+    });
+
+    if (existingHabits.length !== updateHabitsDto.habits.length) {
+      throw new NotFoundException(
+        '일부 습관을 찾을 수 없거나 해당 스터디에 속하지 않습니다.',
+      );
+    }
+
+    const duplicateNames = await this.prisma.habit.findMany({
+      where: {
+        studyId,
+        name: { in: updateHabitsDto.habits.map((habit) => habit.name) },
+        id: { notIn: updateHabitsDto.habits.map((habit) => habit.id) },
+      },
+    });
+
+    if (duplicateNames.length > 0) {
+      throw new ConflictException(
+        `이미 존재하는 습관 이름입니다: ${duplicateNames.map((habit) => habit.name).join(', ')}`,
+      );
+    }
+
+    const updatedHabits = await this.prisma.$transaction(
+      updateHabitsDto.habits.map((habit) =>
+        this.prisma.habit.update({
+          where: { id: habit.id },
+          data: { name: habit.name },
+        }),
+      ),
+    );
+
+    return UpdateHabitsResponseDto.of(updatedHabits);
   }
 }
